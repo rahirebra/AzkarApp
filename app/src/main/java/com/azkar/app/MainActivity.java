a3594149e16bc59af00e72b042f98815acaca662
rahirebra <rahirebra@gmail.com>
2026-06-10 00:35:20 +0300

    Remove appcompat use minimal APK

--- app/src/main/java/com/azkar/app/MainActivity.java
+++ app/src/main/java/com/azkar/app/MainActivity.java
@@ -5,47 +5,27 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.view.Window;
-import android.view.WindowManager;
 import android.graphics.Color;
 
 public class MainActivity extends Activity {
-
     private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Full screen, no title
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
-
-        setContentView(R.layout.activity_main);
-
-        webView = findViewById(R.id.webview);
-
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setAllowFileAccess(true);
-        settings.setAllowFileAccessFromFileURLs(true);
-        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
-
+        WebView webView = new WebView(this);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
         webView.setBackgroundColor(Color.parseColor("#0d1f14"));
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("file:///android_asset/azkar.html");
+        setContentView(webView);
     }
 
     @Override
     public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
     }
 }
